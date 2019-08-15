1a4bc878c349765690ad626b22bbcba86732ad7e
sgidan <sarahgidan@gmail.com>
2019-08-15 16:02:46 -0700

    created login routes

---
+++ client/src/components/Youtube/youtube.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Pageheader from 'react-bootstrap/lib/Pageheader';
+import ResponsiveEmbed from 'react-bootstrap/lib/ResponsiveEmbed';
+import Grid from 'react-bootstrap/lib/Grid';
+
+export default class Services extends React.Component {
+  render() {
+    return (
+        <div id = "services">
+            <Pageheader justified>
+                About us
+                <small>M2 Consulting is Bringing Small Businesses to the Agile century</small>
+                <ResponsiveEmbed a16by9>
+                    <embed src="https://youtu.be/uC9VtVnuPD0"/>
+                </ResponsiveEmbed>
+            </Pageheader>
+        </div>
+    )
+  }
+}
