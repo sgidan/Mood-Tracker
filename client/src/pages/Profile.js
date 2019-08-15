2d1f53c605ec7a60d45f54df6f24c62f9f8a7106
Sheena Brevig <sheena.brevig@gmail.com>
2019-08-15 14:55:47 -0700

    boxes

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -31,44 +31,17 @@ class Profile extends Component {
               <Jumbotron>
                 <h1>Mood Tracker</h1>
               </Jumbotron>
-              <form>
-                <p>I am grateful for...</p>
-                  <Input name="q1" placeholder="1." />
-                  <Input name="title" placeholder="2." />
-                  <Input name="title" placeholder="3." />
-                <p>What would make today great?</p>
-                  <Input name="author" placeholder="1" />
-                  <Input name="title" placeholder="2" />
-                  <Input name="title" placeholder="3" />
-                <p>Daily affirmations:</p>
-                  <Input name="title" placeholder="I am.." />
-                <p>Brain Dump</p>
-                  <TextArea name="synopsis" placeholder="Other notes, ramblings you need to release (Optional)" />
-                <FormBtn>Submit Journal</FormBtn>
-              </form>
+              
             </Col>
-            {/* <Col size="md-6 sm-12">
+            
+           <Col size="md-6 sm-12">
               <Jumbotron>
                 <h1>Books On My List</h1>
               </Jumbotron>
-              {this.state.books.length ? (
-                <List>
-                  {this.state.books.map(book => (
-                    <ListItem key={book._id}>
-                      <a href={"/books/" + book._id}>
-                        <strong>
-                          {book.title} by {book.author}
-                        </strong>
-                      </a>
-                      <DeleteBtn />
-                    </ListItem>
-                  ))}
-                </List>
-              ) : (
-                <h3>No Results to Display</h3>
-              )}
-            </Col> */}
+        
+            </Col>
           </Row>
+         
         </Container>
       );
     }
