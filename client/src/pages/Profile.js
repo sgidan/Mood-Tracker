f277d6d200af3d84610954c517291fb03f094977
Sheena Brevig <sheena.brevig@gmail.com>
2019-08-15 12:47:40 -0700

    in progress

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -25,7 +25,7 @@ class Profile extends Component {
       return (
         <Container fluid>
           <Row>
-            <Col size="md-6">
+            <Col size="md-12">
               <Jumbotron>
                 <h1>Mood Tracker</h1>
               </Jumbotron>
