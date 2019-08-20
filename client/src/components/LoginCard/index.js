e0a246e017da268a5e3540c0aff5b16cebe841e8
Sheena Brevig <sheena.brevig@gmail.com>
2019-08-20 13:35:27 -0700

    debugged after pulling from master

--- client/src/components/LoginCard/index.js
+++ client/src/components/LoginCard/index.js
@@ -21,12 +21,14 @@ export default class LoginCard extends Component {
     API.loginUser({ email, password })
     .then(response => {
         console.log('login user response', response.data);
-        const {_id} = response.data;
-        console.log("login user id", _id)
+        const {_id, name, moods, journals} = response.data;
+        let user = {id:_id, name};
+        console.log("login user id", user)
         // localstorage here but with newly generated mongoID to be used when pulling user profile once redirected to /profile
-        localStorage.clear();
-        localStorage.setItem("userId", JSON.stringify(_id));
+        // localStorage.clear();
+        localStorage.setItem("user", JSON.stringify(user));
     
+        this.props.history.push("/profile");
       })
       .catch(error => {
         if (error) {
@@ -36,7 +38,6 @@ export default class LoginCard extends Component {
       });
       console.log('this.props', this.props);
       console.log('this.props.history', this.props.history);
-      this.props.history.push("/profile");
   };
 
   render() {
