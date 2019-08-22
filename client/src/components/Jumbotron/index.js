decff202341a87d58ebffd5418cea1598c1022a9
sgidan <sarahgidan@gmail.com>
2019-08-22 15:30:58 -0700

    added youtube vids

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,8 +3,14 @@ import React from "react";
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center", background: "#5289B5" }}
-      className="jumbotron"
+      style={{
+        height: 300,
+        clear: "both",
+        paddingTop: 120,
+        textAlign: "center",
+        background: "#5289B5"
+      }}
+      className="embed-responsive embed-responsive-16 by 9"
     >
       {children}
     </div>
@@ -12,4 +18,3 @@ function Jumbotron({ children }) {
 }
 
 export default Jumbotron;
-
